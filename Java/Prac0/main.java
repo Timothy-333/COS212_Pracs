78aba6a982fc2f8c55067ccebfcca058171e2f99
Tim Tam <timothymdevilliers@gmail.com>
2023-02-21 21:44:40 +0200

    Did prac0

---
+++ Java/Prac0/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        student std = new student();
+        std.sayHello();
+    }
+}
---
+++ Java/Prac0/student.java
@@ -0,0 +1,6 @@
+public class student {
+    public void sayHello()
+    {
+        System.out.println("I u22744968 am a COS212 student. I pinky promise that I will work hard in this module and not cheat!!!");
+    }
+}
