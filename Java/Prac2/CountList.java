711c2ef8295302434eb67c085a2b851c5e3b50ba
Tim Tam <timothymdevilliers@gmail.com>
2023-03-08 15:43:53 +0200

    uploading for laptop because of the loadsheding

--- Java/Prac2/CountList.java
+++ Java/Prac2/CountList.java
@@ -9,14 +9,36 @@ public class CountList<T extends Comparable<T>> extends SelfOrderingList<T>{
     public void access(T data) 
     {
         Node<T> nodePtr = head;
-        while(nodePtr != null)
+        if(head != null)
         {
-            if(nodePtr.data.compareTo(data) == 0)
+            while(nodePtr != null)
             {
-                nodePtr.accessCount++;
-                return;
+                if(nodePtr.data.compareTo(data) == 0)
+                {
+                    nodePtr.accessCount++;
+                    break;
+                }
+                nodePtr = nodePtr.next;
             }
-            nodePtr = nodePtr.next;
+            nodePtr = head;
+            while(nodePtr != null)
+            {
+                Node<T> nodePtrTemp = nodePtr;
+                Node<T> checkingNode = nodePtr;
+                while(nodePtrTemp != null && nodePtrTemp.accessCount > nodePtr.next.accessCount)
+                {
+
+                }
+                if(nodePtrTemp != null)
+                {
+                    nodePtrTemp.prev.next = nodePtrTemp.next;
+                    nodePtrTemp.next.prev = nodePtrTemp.prev;
+                    nodePtrTemp.next = nodePtr;
+                    nodePtrTemp.prev = nodePtr.prev;
+                    nodePtr.prev = nodePtrTemp;
+                    nodePtr = nodePtrTemp;
+                }
+            }
         }
     }
 }
