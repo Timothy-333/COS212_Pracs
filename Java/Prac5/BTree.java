af9eb74a8bbbf9db14ec1ba1e6e1ecc2a0e38ac1
Tim Tam <timothymdevilliers@gmail.com>
2023-04-21 15:48:07 +0200

    worked on prac5

--- Java/Prac5/BTree.java
+++ Java/Prac5/BTree.java
@@ -7,8 +7,10 @@ public class BTree<T extends Comparable<T>> {
 	 * 
 	 * @param m
 	 */
-	public BTree(int m) {
-		// TODO - implement BTree.BTree
+	public BTree(int m) 
+	{
+		this.m = m;
+		root = new Node<T>(m);
 		throw new UnsupportedOperationException();
 	}
 
@@ -16,11 +18,54 @@ public class BTree<T extends Comparable<T>> {
 	 * 
 	 * @param data
 	 */
-	public Node<T> insert(T data) {
-		// TODO - implement BTree.insert
+	public Node<T> insert(T data) 
+	{
+		Node<T> currentNode = root;
+		while(currentNode.childrenLength() > 0)
+		{
+			int pos = currentNode.getSmallerKeyPos(data);
+			if(pos == -1)
+				return currentNode;
+			currentNode = currentNode.getChildNode(pos);
+		}
+		if(currentNode.childrenLength() == 0 && currentNode.keysLength() < m-1)
+		{
+			currentNode.setKey(currentNode.getSmallerKeyPos(data)+1, data);
+		}
+		else if(currentNode.childrenLength() == 0 && currentNode.keysLength() == m-1)
+		{
+			currentNode.setKey(currentNode.getSmallerKeyPos(data)+1, data);
+			currentNode = split(currentNode);
+		}
 		throw new UnsupportedOperationException();
 	}
-
+	private Node<T> split(Node<T> node)
+	{
+		Node<T> newNode = new Node<T>(m);
+		Node<T> parentNode = node.parent;
+		if(parentNode == null)
+		{
+			parentNode = new Node<T>(m);
+			root = parentNode;
+		}
+		int pos = parentNode.getSmallerKeyPos(node.getKey(0));
+		parentNode.setKey(pos, node.getKey(0));
+		parentNode.setChildNode(pos, node);
+		parentNode.setChildNode(pos+1, newNode);
+		for(int i = 0; i < m-1; i++)
+		{
+			if(i < (m-1)/2)
+			{
+				newNode.setKey(i, node.getKey((m-1)/2+i+1));
+				node.setKey((m-1)/2+i+1, null);
+			}
+			else
+			{
+				newNode.setKey(i, null);
+			}
+		}
+		return parentNode;
+	}
 	/**
 	 * 
 	 * @param data
--- Java/Prac5/Node.java
+++ Java/Prac5/Node.java
@@ -1,18 +1,62 @@
+import javax.swing.UIDefaults.ProxyLazyValue;
+
 public class Node<T extends Comparable<T>> {
 
 	private Comparable<T>[] keys;
 	private Node<T>[] children;
+	private int m;
+	public Node<T> parent;
 
 	/**
 	 * 
 	 * @param m
 	 */
 	@SuppressWarnings("unchecked")
-	public Node(int m) {
-		// TODO - implement Node.Node
+	public Node(int m) 
+	{
+		keys = new Comparable[m-1];
+		children = new Node[m];
+		this.m = m;
 		throw new UnsupportedOperationException();
 	}
-
+	public Comparable<T> getKey(int i)
+	{
+		return keys[i];
+	}
+	public Node<T> getChildNode(int i)
+	{
+		return children[i];
+	}
+	public int getSmallerKeyPos(T data)
+	{
+		for(int i = 0; i < m-1; i++)
+		{
+			if(keys[i] == null)
+				return i;
+			else if(keys[i].compareTo(data) > 0)
+				return i;
+			else if(keys[i].compareTo(data) == 0)
+				return -1;
+		}
+		return m-1;
+	}
+	public int keysLength()
+	{
+		return keys.length;
+	}
+	public int childrenLength()
+	{
+		return children.length;
+	}
+	public void setKey(int i, Comparable<T> data)
+	{
+		keys[i] = data;
+	}
+	public void setParent(Node<T> parent)
+	{
+		this.parent = parent;
+	}
+	// public void setNode()
 	@Override
 	public String toString() {
 		String res = "[";
